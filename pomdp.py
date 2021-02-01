93e8a138531807f7f53403526d34e0d59177869b
kkkaren <zhoyizhen_1998@hotmail.com>
2021-02-01 21:31:01 +0800

    finitial no initial state

--- pomdp.py
+++ pomdp.py
@@ -1,16 +1,116 @@
-# get inputs
-#inputs = input("enter here : ")
-#mdp_inputs = inputs.split(" ")
 
 #create an initial dictionary for belief state
-bs_dic = dict()
 states = [(x,y) for x in range(1,5) for y in range(1,4)]
-bs_dic = { a: (1/9) for a in states }
-del bs_dic [(2,2)]
-bs_dic[(4,1)] = 0
-bs_dic[(4,2)] = 0
+states.remove((2,2))
+b = { a: (1/9) for a in states }
+b[(4,3)] = 0
+b[(4,2)] = 0
 
 #create a sensor model 
-obser = dict()
 sensor = [(e,s) for e in range(1,3) for s in states]
-print(sensor)
+obs = { a: 0 for a in sensor}
+for s in sensor:
+    if s[1] == (4,3) or s[1] == (4,2):
+        obs[s] = 0
+    else:
+        if s[0] == 1:
+            if s[1][0] == 3:
+                obs[s] = 0.9
+            else :
+                obs[s] = 0.1
+        if s[0] == 2:
+            if s[1][0] == 3:
+                obs[s] = 0.1
+            else:
+                obs[s] = 0.9
+
+#create a dictionary for action and state
+a_s = {a:list() for a in states}
+for s in states:
+    us = (s[0],s[1]+1)
+    ds = (s[0],s[1]-1)
+    rs = (s[0]+1,s[1])
+    ls = (s[0]-1,s[1])
+    for a in ("up","down","left","right"):
+        if a == "up":
+            if us in states:
+                a_s[us].append((a,s,0.8))
+            else:
+                a_s[s].append((a,s,0.8))
+            if rs in states:
+                a_s[rs].append((a,s,0.1))
+            else:
+                a_s[s].append((a,s,0.1))
+            if ls in states:
+                a_s[ls].append((a,s,0.1))
+            else:
+                a_s[s].append((a,s,0.1))
+        if a == "down":
+            if ds in states:
+                a_s[ds].append((a,s,0.8))
+            else:
+                a_s[s].append((a,s,0.8))
+            if rs in states:
+                a_s[rs].append((a,s,0.1))
+            else:
+                a_s[s].append((a,s,0.1))
+            if ls in states:
+                a_s[ls].append((a,s,0.1))
+            else:
+                a_s[s].append((a,s,0.1))
+        if a == "right":
+            if rs in states:
+                a_s[rs] .append((a,s,0.8))
+            else:
+                a_s[s].append((a,s,0.8))
+            if ds in states:
+                a_s[ds].append((a,s,0.1))
+            else:
+                a_s[s].append((a,s,0.1))
+            if us in states:
+                a_s[us].append((a,s,0.1))
+            else:
+                a_s[s] .append((a,s,0.1))
+        if a == "left":
+            if ls in states:
+                a_s[ls] .append((a,s,0.8))
+            else:
+                a_s[s] .append((a,s,0.8))
+            if ds in states:
+                a_s[ds].append((a,s,0.1))
+            else:
+                a_s[s] .append( (a,s,0.1))
+            if us in states:
+                a_s[us] .append((a,s,0.1))
+            else:
+                a_s[s].append((a,s,0.1))
+
+#get inputs
+inputs = input("enter here : ")
+inputs = inputs.split(" ")
+init = inputs[0]
+actions = inputs[1]
+actions = actions.split(",")
+evids = inputs[2]
+evids = evids.split(",")
+evids = [ int(x) for x in evids]
+
+
+for i in range(len(actions)):
+        #for all b(s)
+    new_b = dict()
+    for s in b:
+        prob = 0
+            #probability of all the states that can reach s
+            #with action a times b(s)
+        for p in a_s[s]:
+            if p[0] == actions[i]:
+                prob += p[2]*b[p[1]]
+        new_b[s] = obs[(evids[i],s)]*prob
+    norm = sum(new_b.values())
+    for s in new_b:
+        new_b[s] = new_b[s]/norm
+    b = new_b
+
+print(b)
+
